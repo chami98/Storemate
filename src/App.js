579a1d3afff9e0f749170cad01b991f943679f53
chami98 <chamikara98@gmail.com>
2024-03-14 22:25:26 +0530

    Add product JSON | Create Product Container |

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ProductCard from './Components/ProductCard';
+import ProductContainer from './Components/ProductContainer';
 
 function App() {
   return (
     <>
-      <ProductCard name="IPhone" price={9.99} quantity={10} imageUrl="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/fall-2023-iphone-colors-iphone-15-pro-max.png" />
+      <ProductContainer />
     </>
   );
 }
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -6,12 +6,22 @@ import Typography from '@mui/material/Typography';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
-export default function ProductCard({ name, price, quantity, imageUrl }) {
+export default function ProductCard({ name, price, quantity, imageUrls }) {
+
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [imageUrls.length]);
   return (
-    <Card sx={{ maxWidth: 345, boxShadow: '0px 3px 10px rgba(0,0,0,0.1)' }}>
+    <Card sx={{ maxWidth: 345, margin: 'auto', boxShadow: '0px 3px 10px rgba(0,0,0,0.1)' }}>
       <CardMedia
         sx={{ height: 200 }}
-        image={imageUrl}
+        image={imageUrls[currentImageIndex]}
         title="Product Image"
       />
       <CardContent>
